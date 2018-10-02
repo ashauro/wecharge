70a30668e2b6e8675de894e2558c088d540188f5
Alexey Shauro <a.shauro@gmail.com>
2018-10-03 00:30:51 +0300

    Model and admin update

--- chargemap/admin.py
+++ chargemap/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import ChargeStation, Address, ChargeStationAvailability, ChargeStationPlugType
+
+admin.site.register (ChargeStation)
+admin.site.register (Address)
+admin.site.register (ChargeStationAvailability)
+admin.site.register (ChargeStationPlugType)
--- chargemap/models.py
+++ chargemap/models.py
@@ -1,3 +1,50 @@
 from django.db import models
 
 # Create your models here.
+
+# Типы разъемов
+class ChargeStationPlugType (models.Model):
+    plug_type_1 = models.CharField(max_length=100)
+    plug_type_2 = models.CharField(max_length=100)
+    # Схема разъема (картинка) - не реализовано
+
+# Когда доступна (24/7, по рабочим дням и пр.)
+class ChargeStationAvailability (models.Model):
+    charge_station_available = models.CharField(default='No data', max_length=100)
+    charge_station_not_available = models.CharField(default='No data', max_length=100)
+    charge_station_not_data = models.CharField(default='No data', max_length=100)
+
+# Описание зарядной станции
+class ChargeStation (models.Model):
+    # Наименование места (ТЦ ХХХ, Заправка Лукойл и пр.)
+    charge_station_name = models.CharField(max_length=100)
+    # Оператор (Моэск и пр)
+    charge_station_operator = models.CharField(max_length=100)
+    # Мощность, кВт
+    charge_station_maxpower = models.IntegerField()
+    # Тип разъема
+    charge_station_plug = models.ForeignKey(ChargeStationPlugType, default='0', on_delete=models.CASCADE)
+    # Статус: (свободна/забронирована/идет заправка)
+    charge_station_status = models.ForeignKey(ChargeStationAvailability, default='0', on_delete=models.CASCADE)
+    # Оценка заправки (оценить удобство использования)
+    charge_station_score = models.FloatField()
+    # Когда доступна (24/7, по рабочим дням и пр.)
+    # Описание (пример: Находится на парковке ТЦ Globus, прямо у входа (дальний от Москвы). 
+    # Доступ свободный. Открывается картой МОЭСК)
+    charge_station_description = models.TextField(max_length=200)
+
+    def __str__(self):
+        return self.charge_station_name
+
+# Адреса
+class Address (models.Model):
+    charge_station = models.ForeignKey(ChargeStation, on_delete=models.CASCADE)
+    country = models.CharField(max_length=100)
+    city = models.CharField(max_length=100)
+    street = models.CharField(max_length=100)
+    house_number = models.CharField(max_length=100)
+
+#Фото заправки - не реализовано
+#Стоимость заправки (тарифы) - не реализовано
+#Способы оплаты (карта оператора, NFC, кредитная карта) - не реализовано
+    
