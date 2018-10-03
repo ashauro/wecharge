5c977c99c56fe15d8c86f94442dad43e6619bc64
Helga Rais <cdg013@gmail.com>
2018-10-04 00:48:31 +0300

    Моя попытка сделать правильную выгрузку в json из базы, не полностью

--- chargemap/dojson.py
+++ chargemap/dojson.py
@@ -1,9 +1,17 @@
 from django.core import serializers
-import models
+from .models import ChargeStation
 
-data = serializers.serialize("xml", ChargeStation.objects.all())
+json_dict = {
+    "type": "FeatureCollection",
+    "features": [
+        {"type": "Feature", "id": 0, "geometry": {"type": "Point", "coordinates": [55.831903, 37.411961]}},
+        {"type": "Feature", "id": 1, "geometry": {"type": "Point", "coordinates": [55.763338, 37.565466]}},
+        {"type": "Feature", "id": 2, "geometry": {"type": "Point", "coordinates": [55.763338, 37.616378]}}
+    ]
+}
+model_station = ChargeStation.objects.all()
+for station in model_station:
+    print(station.charge_station_latitude)
 
 with open ('json.txt', 'w', encoding = 'utf-8') as file:
-    file.write(data)
-
-
+    file.write(model_station)
--- chargemap/views.py
+++ chargemap/views.py
@@ -7,11 +7,18 @@ from chargemap.models import ChargeStation
 # Create your views here.
 
 def dojson(request):
-	data = serializers.serialize("json", ChargeStation.objects.all())
-	with open ('AllStations.json', 'w', encoding = 'utf-8') as file:
-		file.write(data)
-
-	return HttpResponse("JSON is DONE!")
+    json_dict = {
+        "type": "FeatureCollection",
+        "features": [
+            {"type": "Feature", "id": 0, "geometry": {"type": "Point", "coordinates": [55.831903, 37.411961]}},
+            {"type": "Feature", "id": 1, "geometry": {"type": "Point", "coordinates": [55.763338, 37.565466]}},
+            {"type": "Feature", "id": 2, "geometry": {"type": "Point", "coordinates": [55.763338, 37.616378]}}
+        ]
+    }
+    model_station = ChargeStation.objects.all()
+    for station in model_station:
+        print(station.charge_station_latitude)
+    return HttpResponse("JSON is DONE!")
 
 
 def index(request):
