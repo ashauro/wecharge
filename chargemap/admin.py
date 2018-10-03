eeb4c6ac13287e714a0fa2522a68afd2a7b2ca5e
Alexey Shauro <a.shauro@gmail.com>
2018-10-03 19:22:05 +0300

    Updated data model

--- chargemap/admin.py
+++ chargemap/admin.py
@@ -2,9 +2,11 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import ChargeStation, Address, ChargeStationAvailability, ChargeStationPlugType
+from .models import ChargeStation, Address, ChargeStationWorkingTime, ChargeStationPlugType, ChargeStationStatus, ChargeStationType
 
 admin.site.register (ChargeStation)
 admin.site.register (Address)
-admin.site.register (ChargeStationAvailability)
+admin.site.register (ChargeStationWorkingTime)
 admin.site.register (ChargeStationPlugType)
+admin.site.register (ChargeStationStatus)
+admin.site.register (ChargeStationType)
--- chargemap/models.py
+++ chargemap/models.py
@@ -2,43 +2,65 @@ from django.db import models
 
 # Create your models here.
 
+# Типы зарядки
+class ChargeStationType (models.Model):
+    station_type = models.CharField(max_length=100)
+    # Схема разъема (картинка) - не реализовано
+    def __str__(self):
+        return self.station_type
+
 # Типы разъемов
 class ChargeStationPlugType (models.Model):
-    plug_type_1 = models.CharField(max_length=100)
-    plug_type_2 = models.CharField(max_length=100)
+    plug_type = models.CharField(max_length=100)
     # Схема разъема (картинка) - не реализовано
+    def __str__(self):
+        return self.plug_type
 
-# Когда доступна (24/7, по рабочим дням и пр.)
-class ChargeStationAvailability (models.Model):
-    charge_station_available = models.CharField(default='No data', max_length=100)
-    charge_station_not_available = models.CharField(default='No data', max_length=100)
-    charge_station_not_data = models.CharField(default='No data', max_length=100)
+# Статус зарядки
+class ChargeStationStatus (models.Model):
+    status = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.status
+
+# Рабочие часы зарядки
+class ChargeStationWorkingTime (models.Model):
+     working_time = models.CharField(max_length=100, default=None)
+     def __str__(self):
+        return self.working_time
 
 # Описание зарядной станции
 class ChargeStation (models.Model):
     # Наименование места (ТЦ ХХХ, Заправка Лукойл и пр.)
     charge_station_name = models.CharField(max_length=100)
+    # Координаты 
+    charge_station_latitude = models.FloatField(max_length=10, default=0)
+    harge_station_longtitude = models.FloatField(max_length=10, default=0)
+    # Тип зарядки
+    charge_station_type = models.ForeignKey(ChargeStationType, on_delete=models.CASCADE, default=None)
     # Оператор (Моэск и пр)
     charge_station_operator = models.CharField(max_length=100)
     # Мощность, кВт
     charge_station_maxpower = models.IntegerField()
     # Тип разъема
-    charge_station_plug = models.ForeignKey(ChargeStationPlugType, default='0', on_delete=models.CASCADE)
+    charge_station_plug = models.ManyToManyField(ChargeStationPlugType)
     # Статус: (свободна/забронирована/идет заправка)
-    charge_station_status = models.ForeignKey(ChargeStationAvailability, default='0', on_delete=models.CASCADE)
+    charge_station_status = models.ForeignKey(ChargeStationStatus, on_delete=models.CASCADE, default=None)
     # Оценка заправки (оценить удобство использования)
     charge_station_score = models.FloatField()
-    # Когда доступна (24/7, по рабочим дням и пр.)
+    # Рабочие часы (24/7, по рабочим дням и пр.)
+    charge_station_working_time = models.ForeignKey(ChargeStationWorkingTime, on_delete=models.CASCADE, default=None)
+
     # Описание (пример: Находится на парковке ТЦ Globus, прямо у входа (дальний от Москвы). 
     # Доступ свободный. Открывается картой МОЭСК)
-    charge_station_description = models.TextField(max_length=200)
+    charge_station_description = models.TextField(max_length=300)
+    
 
     def __str__(self):
         return self.charge_station_name
 
 # Адреса
 class Address (models.Model):
-    charge_station = models.ForeignKey(ChargeStation, on_delete=models.CASCADE)
     country = models.CharField(max_length=100)
     city = models.CharField(max_length=100)
     street = models.CharField(max_length=100)
