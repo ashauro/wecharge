60237f939309d9e37e55383a968550d111ef9878
Alexey Shauro <a.shauro@gmail.com>
2018-10-04 00:19:40 +0300

    эксперименты с генерацией json

--- chargemap/views.py
+++ chargemap/views.py
@@ -6,12 +6,25 @@ from chargemap.models import ChargeStation
 
 # Create your views here.
 
-def dojson(request):
-	data = serializers.serialize("json", ChargeStation.objects.all())
-	with open ('AllStations.json', 'w', encoding = 'utf-8') as file:
-		file.write(data)
+#def dojson(request):
+#	data = serializers.serialize("json", ChargeStation.objects.all())
+#	with open ('AllStations.json', 'w', encoding = 'utf-8') as file:
+#		file.write(data)
+#
+#	return HttpResponse("JSON is DONE!")
 
-	return HttpResponse("JSON is DONE!")
+
+def dojson(request):
+	json = ''
+	i = 1
+	for station in ChargeStation.objects.all():
+		station = ChargeStation.objects.get(id=i)
+		json = { "type": "FeatureCollection", "features": [{"type": "Feature", "id": i, "geometry": {"type": "Point", "coordinates": [station.charge_station_latitude, station.harge_station_longtitude]}}]}
+		i += 1
+		#json = {"a":1, "b":2, "c":3}
+		print (json)
+		return HttpResponse(json)
+
 
 
 def index(request):
