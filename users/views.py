9629a4871a04e1a4665b37ff78961059fbfbad7f
Alexey Shauro <a.shauro@gmail.com>
2018-10-19 22:29:18 +0300

    сделал вывод профиля

--- users/views.py
+++ users/views.py
@@ -5,7 +5,4 @@ from users.models import Profile, Car
 # Create your views here.
 
 def show_profile(request):
-    to_page = ''
-    for profile in Profile.objects.all():
-        to_page += str(profile.user_city)
     return render(request, 'profile.html')
