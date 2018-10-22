9cb42c449d6d585d49cd8cec46eb4bfbd42b8a84
Alexey Shauro <a.shauro@gmail.com>
2018-10-22 23:48:37 +0300

    доработал модель пользователя

--- users/admin.py
+++ users/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Profile, Car
+from .models import Profile, Car, Billing
 
 # Register your models here.
 
 admin.site.register(Profile)
 admin.site.register(Car)
+admin.site.register(Billing)
--- users/models.py
+++ users/models.py
@@ -5,6 +5,7 @@ from PIL import Image
 
 # Create your models here.
 
+# Данные об автомобиле
 class Car (models.Model):
    name = models.CharField(max_length=100, null=True)
    plate = models.CharField(max_length=9, null=True)
@@ -12,13 +13,14 @@ class Car (models.Model):
    def __str__(self):
        return self.name
 
+# Профиль пользователя
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     user_image = models.ImageField(default='default.png', upload_to='users/profile_pics')
     user_country = models.ForeignKey(Country, null=True, on_delete=models.CASCADE)
     user_city = models.ForeignKey(City, null=True, on_delete=models.CASCADE)
     user_car = models.ForeignKey(Car, null=True, on_delete=models.CASCADE)
-    
+    user_driving_license = models.CharField(max_length=100, null=True)
     def __str__(self):
         return self.user.username
     
@@ -32,3 +34,13 @@ class Profile(models.Model):
              img.thumbnail(output_size)
              img.save(self.user_image.path)
 
+# Информация о состоянии счета
+class Billing(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    number_of_charges = models.IntegerField(null=True, default=0)
+    cunsumed_power = models.FloatField(max_length=10, null=True, default=0)
+    balance = models.FloatField(max_length=10, null=True, default=0)
+    bonus_balance = models.FloatField(max_length=10, null=True, default=0)
+    
+    def __str__(self):
+       return self.user.username
